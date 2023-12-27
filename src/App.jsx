2ed3374d6847e2bce4a6025fef1f3eed23b7c1ee
naveed60 <advmiannaveedali@gmail.com>
2023-12-27 17:14:36 +0500

    sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import Header from "./components/Header"
 import UserInput from "./components/UserInput"
 import Results from "./components/Results";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   const [userInput, setUserInput]= useState({
